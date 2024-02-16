da97d21689fd4d1b47295ffad629eab5b81bef61
Archana <kumariarchana98013@gmail.com>
2024-02-16 21:04:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import Card from './Component/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  let data = [{
+    title: "FREE",
+    price: "$0",
+    user: "Single User",
+    storage: "5GB Storage",
+    domain: "Free Subdomain",
+    project: "text-muted",
+    projectIcon: "fas fa-times",
+    phone: "text-muted",
+    phoneIcon: "fas fa-times",
+    subDomain: "text-muted",
+    subDomainIcon: "fas fa-times",
+    monthly: "text-muted",
+    monthlyIcon: "fas fa-times"
+  },
+  {
+    title: "PLUS",
+    price: "$9",
+    user: "5 Users",
+    storage: "50GB Storage",
+    domain: "Free Subdomain",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "text-muted",
+    monthlyIcon: "fas fa-times"
+  },
+  {
+    title: "PRO",
+    price: "$49",
+    user: "Unlimited Users",
+    storage: "150GB Storage",
+    domain: "Unlimited Free Subdomain",
+    project: "",
+    projectIcon: "fas fa-check",
+    phone: "",
+    phoneIcon: "fas fa-check",
+    subDomain: "",
+    subDomainIcon: "fas fa-check",
+    monthly: "",
+    monthlyIcon: "fas fa-check"
+  }];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Header />
+      <section className="pricing py-5 bg-primary">
+        <div className="container">
+          <div className="row">
+            {
+              data.map((e, i) => {
+                return <Card cardData={e} key={i} />
+              })
+            }
+          </div>
+        </div>
+      </section>
+
+      
     </div>
   );
 }
---
+++ src/Component/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Header() {
+ 
+
+  return (
+    <div>
+        <div className='head1'>
+            <p>Price Card</p>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
+
+ 
